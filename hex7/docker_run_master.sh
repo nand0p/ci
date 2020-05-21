5b7c96a7fda4d6b56c22bc6f8c1743886ca05f60
nando <nando@hex7.com>
2020-05-21 05:53:00 +0000

    moar

--- hex7/docker_run_master.sh
+++ hex7/docker_run_master.sh
@@ -1,14 +1,16 @@
-#!/bin/sh 
+#!/bin/bash -ex
 
-docker network create -d bridge buildbot || true
-
-if [ "$1" == "kill" ]; then
-  docker kill buildbot-hex7
-fi
-
+docker network create -d bridge buildbot 2> /dev/null || true
+docker container rm buildbot-hex7 2> /dev/null || true
+docker kill buildbot-hex7 2> /dev/null || true
 
 docker build -t buildbot-hex7 -f Dockerfile.master .
-docker run --rm --name buildbot-hex7 --network=buildbot -d -p 9989:9989 -p 8010:8010 buildbot-hex7
 
-sleep 5
-docker ps
+if [ "$1" == "console" ]; then
+  echo "docker run --rm --name buildbot-hex7 --network=buildbot -ti -p 9989:9989 -p 8010:8010 buildbot-hex7 bash"
+else
+  docker run --rm --name buildbot-hex7 --network=buildbot -d -p 9989:9989 -p 8010:8010 buildbot-hex7
+  sleep 5
+  docker ps
+fi
+
