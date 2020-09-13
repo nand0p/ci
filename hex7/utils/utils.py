55544c67681117a7510e3d888dccddec431c4316
nando <nando@hex7.com>
2020-09-12 21:17:05 -0400

    moar utils

--- hex7/utils/utils.py
+++ hex7/utils/utils.py
@@ -5,3 +5,25 @@ def get_change_source(repo, branch, poll):
                            workdir=repo + '-workdir',
                            branch=branch,
                            pollInterval=poll)
+
+
+def get_branch_scheduler(name, branch):
+  return schedulers.SingleBranchScheduler(name=name,
+                                          change_filter=util.ChangeFilter(branch=branch),
+                                          treeStableTimer=None,
+                                          builderNames=[name])
+
+
+def get_force_scheduler(name):
+  scheduler_name = 'force-' + name
+  return schedulers.ForceScheduler(name=scheduler_name,
+                                   builderNames=[name])
+
+
+def get_nightly_scheduler(name, branch, hour, minute):
+  scheduler_name = 'nightly-' + name
+  return schedulers.Nightly(name=scheduler_name,
+                            branch=branch,
+                            builderNames=[name],
+                            hour=hour,
+                            minute=minute)
