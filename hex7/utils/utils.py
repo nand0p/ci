a2c0c0778d57e83b295e482dfeff9437f325b93c
nando <nando@hex7.com>
2020-09-12 22:07:02 -0400

    changesource

--- hex7/utils/utils.py
+++ hex7/utils/utils.py
@@ -1,8 +1,10 @@
 from buildbot.plugins import *
 
+
 def get_change_source(repo, branch, poll):
-  return changes.GitPoller('https://github.com/nand0p/' + repo + '/covid19.git',
-                           workdir=repo + '-workdir',
+  workdir = repo + '-workdir'
+  return changes.GitPoller('https://github.com/nand0p/' + repo + '.git',
+                           workdir=workdir,
                            branch=branch,
                            pollInterval=poll)
 
